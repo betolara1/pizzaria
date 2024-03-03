caa05eecb2fd382418fa05cbd69554f8e42b8204
Roberto Lara <robertokantovitzlara@hotmail.com>
2024-03-02 23:29:09 -0300

    continuando mexendo nos html para as paginas de contatos e informações

--- homepage/views.py
+++ homepage/views.py
@@ -4,3 +4,6 @@ def homepage(request):
     if request.method == 'GET':
         return render(request, 'homepage.html')
     
+def contatos(request):
+    if request.method == 'GET':
+        return render(request, 'contatos.html')
--- pizzaria/settings.py
+++ pizzaria/settings.py
@@ -118,7 +118,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
+STATIC_ROOT = os.path.join('static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+
+from django.contrib.messages import constants
+
+MESSAGE_TAGS = {
+    constants.DEBUG: 'alert-primary',
+    constants.ERROR: 'alert-danger',
+    constants.SUCCESS: 'alert-success',
+    constants.INFO: 'alert-info',
+    constants.WARNING: 'alert-warning',
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
